4367696f7fab67cbd5dd5e8e78e854f4bb2317f3
predat-r <harisnaeem612@outlook.com>
2024-09-07 17:19:21 +0500

    added cart item counter

---
+++ src/components/CardItemCount.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect } from "react-redux";
+function CardItemCount({ CartItemCount }) {
+  return (
+    <div className="size-3 md:size-5 rounded-full bg-[#9AE5C9] flex absolute left-[1.35vw]  md:left-[0.7vw] text-xs md:text-sm text-[#1A9E6F] bottom-8 md:bottom-6 text-center items-center justify-center">
+      {CartItemCount}
+    </div>
+  );
+}
+const mapStateToProps = (state) => ({
+  CartItemCount: state.cart.cart.length,
+});
+export default connect(mapStateToProps, null)(CardItemCount);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CardItemCount from "./CardItemCount";
 function Navbar() {
   return (
     <div className="flex flex-row bg-bars w-full h-16 rounded-lg justify-between p-5 shadow-elevate  mb-5 items-center ">
-      <button className=" size-5 sm:size-9">
-       
-        <Link to="/Cart"> <img src="src/assets/carticon-white.png"></img></Link>
-      </button>
+      <Link to="/Cart">
+        {" "}
+        <button className="relative flex flex-col  w-10 h-10">
+          <div className=" size-5 sm:size-9">
+            <img src="src/assets/carticon-white.png"></img>
+          </div>
+          <CardItemCount></CardItemCount>
+        </button>
+      </Link>
       <h1 className="text-lg sm:text-3xl font-bold text-white ml-9">
         {" "}
         Florify 🌱
